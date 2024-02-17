bd157b93dacc40fac53008d55f351767a53696b9
Oskar Kraak <74422644+oskarkraak@users.noreply.github.com>
2024-02-17 12:11:10 -0800

    Implement saving to database

--- GenBridge-Frontend/genbridge-react/src/MatchPage.js
+++ GenBridge-Frontend/genbridge-react/src/MatchPage.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { useLocation } from 'react-router-dom';
 
 const MatchPage = () => {
     const location = useLocation();
-    const { name, role } = location.state;
+    const data = location.state;
+
+    useEffect(() => {
+        // Sign up
+        async function fetchData(data) {
+            try {
+                const response = await fetch('https://genbridge-413824882f14.herokuapp.com/signup/', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(data),
+                });
+
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+
+                const result = await response.json();
+                console.log(result);
+            } catch (error) {
+                console.error('There was an error!', error);
+            }
+        }
+
+        fetchData(data);
+    },[]);
 
     return (
         <div>
             <h1>You have been matched with...</h1>
-            <p>Name: {name}</p>
-            <p>Role: {role}</p>
-            {/* Display other info here */}
+            <p>Name: {data.name}</p>
+            <p>Role: {data.senior}</p>
         </div>
     );
 };
