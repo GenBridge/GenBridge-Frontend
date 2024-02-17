8dae647e18cbfaff9fa3a01389c2abbccf65660b
Oskar Kraak <74422644+oskarkraak@users.noreply.github.com>
2024-02-17 15:33:23 -0800

    Display match result on MatchPage

--- GenBridge-Frontend/genbridge-react/src/MatchPage.js
+++ GenBridge-Frontend/genbridge-react/src/MatchPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 const MatchPage = () => {
@@ -6,8 +6,10 @@ const MatchPage = () => {
     const navigate = useNavigate();
     const data = location.state;
 
+    // Initialize state to store the fetched result
+    const [result, setResult] = useState(null);
+
     useEffect(() => {
-        // Sign up
         async function fetchData(data) {
             try {
                 const response = await fetch('https://genbridge-413824882f14.herokuapp.com/signup/', {
@@ -24,11 +26,12 @@ const MatchPage = () => {
 
                 const result = await response.json();
                 console.log(result);
+                // Store the fetched result in state
+                setResult(result);
             } catch (error) {
                 console.error('There was an error!', error);
             }
         }
-
         fetchData(data);
     }, [data]);
 
@@ -39,9 +42,18 @@ const MatchPage = () => {
     return (
         <div>
             <h1>You have been matched with...</h1>
-            <p>Name: {data.name}</p>
-            <p>Role: {data.senior}</p>
-            <button onClick={redirectToCalendlyLinkPage}>Enter Calendly Link</button> {}
+            {/* Ensure `result` is not null before trying to access its properties */}
+            {result ? (
+                <>
+                    <p>Name: {result.match.user.name}</p>
+                    <p>Role: {result.match.user.senior ? 'Senior' : 'Junior'}</p>
+                    {/* Add more properties as needed */}
+
+                    <button onClick={redirectToCalendlyLinkPage}>Enter Calendly Link</button> {}
+                </>
+            ) : (
+                <p>Loading...</p> // Display loading text or spinner while waiting for the fetch to complete
+            )}
         </div>
     );
 };
